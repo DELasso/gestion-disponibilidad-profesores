6e7a36ce419ad961f43ee929c59fe82b2918f5b1
D.Lasso <davidlh2005@gmail.com>
2025-04-12 16:22:07 -0500

    Primet commit: estructura monorepo y Hola Mundo en frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bienvenido al Sistema de Gestión de Disponibilidad de Profesores</h1>
+      <p>Este sistema permite a estudiantes y docentes. Gestionar o consultar disponibilidad para tutorías.</p>
+    </div>
+  );
+}
+
+export default App;
